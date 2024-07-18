9663622b8814acd102f663a3adb32113ab05effd
rikiaaan <rikiaaan7101pr@outlook.com>
2024-07-18 09:36:31 +0900

    実際にfsでファイルの名前を変更する方式から、ファイルのパスをVectorに格納する方式に変更

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use rand::{self, Rng};
 
 fn main() {
 
-	println!("rikiaaan-terminal-background-image-changer v1.0.0");
+	println!("rikiaaan-terminal-background-image-changer v1.1.0");
 
 	let started_time = SystemTime::now();
 
 	let args = {
-		let mut args: Vec<OsString> = env::args_os().collect();
+		let args: Vec<OsString> = env::args_os().collect();
 		if args.len() != 3 {
 			eprintln!("[ERROR] invalid arg counts.");
 			exit(-1);
@@ -39,11 +39,10 @@ fn main() {
 		path_buf
 	};
 
-	let uuid_v4 = Uuid::new_v4();
+	let mut file_entry: Vec<PathBuf> = Vec::new();
 
-	let mut file_counts: usize = 0;
-
-	let current_uuid_regex = Regex::new(uuid_v4.to_string().as_str()).unwrap();
+	// /\.jpg$/ とか /\.png$/ とか
+	let extension_regex = Regex::new(format!("\\.{}$", image_extension).as_str()).unwrap();
 
 	fs::read_dir(&background_source_dir)
 		.unwrap()
@@ -51,33 +50,38 @@ fn main() {
 			let file = file.unwrap();
 			let file_name = file.file_name();
 
-			if current_uuid_regex.is_match(file_name.to_str().unwrap()) {
-				return ();
+			// もし指定した拡張子に合っていなかったら
+			if !extension_regex.is_match(file_name.to_str().unwrap()) {
+				// continue的なreturn
+				return;
 			}
 
-			let file_rename_path = (&background_source_dir).join(format!("{}_{}.{}", uuid_v4, file_counts, image_extension));
-
-			fs::rename(file.path(), file_rename_path);
-
-			file_counts += 1;
+			file_entry.push(file.path());
 		});
 
-	if file_counts < 2 {
+	let file_entry_len = file_entry.len();
+
+	if file_entry_len < 2 {
 		eprintln!("[ERROR] at least 2 images are required");
 		exit(-1);
 	}
 
-	let random_number = rand::thread_rng().gen_range(0..file_counts);
+	let random_number = rand::thread_rng().gen_range(0..file_entry_len);
 
 	let image_dist_path = (&background_dir).join(format!("image.{}", image_extension));
-	let image_target_path = (&background_source_dir).join(format!("{}_{}.{}", uuid_v4, random_number, image_extension));
+	let image_target_path = &file_entry[random_number];
 
 	if (&image_dist_path).exists() {
-		let last_image_path = background_source_dir.join(format!("last_image.{}", image_extension));
-		fs::rename(&image_dist_path, &last_image_path);
+		let uuid = Uuid::new_v4();
+		let last_image_path = background_source_dir.join(format!("{}_last_image.{}", uuid, image_extension));
+		if let Err(err) = fs::rename(&image_dist_path, &last_image_path) {
+			eprintln!("{:?}", err);
+		}
 	}
 
-	fs::rename(&image_target_path, &image_dist_path);
+	if let Err(err) = fs::rename(image_target_path, &image_dist_path) {
+		eprintln!("{:?}", err);
+	}
 
 	let elapsed = SystemTime::now().duration_since(started_time).unwrap();
 	println!("finished: {}ms", elapsed.as_millis());
